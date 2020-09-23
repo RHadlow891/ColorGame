18b25162c5e7492e8ff2d1fc2636e73c86a6ec05
Ryan Hadlow <rhadlow@outlook.com>
2020-09-23 22:55:39 +0100

    Initial Commit

---
+++ Sync the city/demo.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        #bg {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(233, 233, 233, 1.0);
+        }
+        #sp {
+            margin: 10px;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(102, 210, 201, 0.2);
+        }
+    </style>
+</head>
+<body>
+    <div class="bg">
+
+        <span class="rn"></span>
+
+        <span class="sp"></span>
+
+        <span class="ln"></span>
+
+    </div>
+</body>
+</html>
